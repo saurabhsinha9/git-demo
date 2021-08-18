933508efd5ddb618beec6c3ed7fa68a7111d2e32
saurabhsinha9 <saurabhsinha9@gmail.com>
2021-08-18 16:30:33 +0530

    JDBCUtil added

---
+++ JDBCUtil.java
@@ -0,0 +1,10 @@
+public class JDBCUtil{
+	private static final DRIVER_NAME="";
+	private static final URL="";
+	private static final USERNAME="";
+	private static final PASSWORD="";
+	private static Connection connection;
+	public static Connection getConnection(){
+		return connection;
+	}
+}	
